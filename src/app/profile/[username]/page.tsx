d29352ac33f05f2d3039397b41e0d0c5f4bff377
Prudvi <prudvirajvarma2004@gmail.com>
2025-03-04 11:24:11 +0530

    Added profile actions

---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function getMetadata(){
+    
+}
+
+function ProfilePage({params} : {params : {username : string}}) {
+  return (
+    
+    <div>Profile Page</div>
+  )
+}
+
+export default ProfilePage
