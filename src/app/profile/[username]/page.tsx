46f5acfddb72c39ea5a376728db5879785769506
Prudvi <prudvirajvarma2004@gmail.com>
2025-03-04 12:07:21 +0530

    Added profile and made it editable

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,14 +1,35 @@
+import { getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing } from '@/actions/profile.action'
+import { notFound } from 'next/navigation'
 import React from 'react'
+import ProfilePageClient from './ProfilePageClient'
 
-function getMetadata(){
-    
+export async function generateMetadata({ params }: { params: { username: string } }) {
+    const user = await getProfileByUsername(params.username)
+    if (!user) return
+
+    return {
+        title: `${user.name ?? user.username}`,
+        description: user.bio || `Check Out ${user.username}'s profile`
+    }
 }
 
-function ProfilePage({params} : {params : {username : string}}) {
-  return (
-    
-    <div>Profile Page</div>
-  )
+async function ProfilePage({ params }: { params: { username: string } }) {
+    const user = await getProfileByUsername(params.username)
+    if (!user) return notFound()
+
+    const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+        getUserPosts(user.id),
+        getUserLikedPosts(user.id),
+        isFollowing(user.id)
+    ])
+    return (
+        <ProfilePageClient 
+            user = {user}
+            posts = {posts}
+            likedPosts = {likedPosts}
+            isFollowing = {isCurrentUserFollowing}
+        />
+    )
 }
 
 export default ProfilePage
